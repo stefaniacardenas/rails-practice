ff504245e0fb7998cf135f462bd174603ae01c6b
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-05-19 17:51:49 +0100

    Added a restaurants#index with no restaurants added yet

--- my_yelp/spec/features/restaurant_feature_spec.rb
+++ my_yelp/spec/features/restaurant_feature_spec.rb
@@ -1,2 +1,12 @@
 require 'spec_helper.rb'
 
+describe "restaurant index page" do 
+	context 'when there are no restaurants'
+	
+	it 'should show a message' do 
+		visit '/restaurants'
+		expect(page).to have_content('No restaurants added yet!')
+	end
+
+end
+
